015f45bb38ab9046e578ed16f695a25eaa0008cf
NayanTheSpaceGuy <147062173+NayanTheSpaceGuy@users.noreply.github.com>
2024-01-05 01:34:04 +0530

    Added Tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nayanthespaceguy.github.io',
+  site: 'https://nayanthespaceguy.github.io',
+  integrations: [tailwind()]
 });
